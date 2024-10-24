0528246a6fe9bd5bc3be667e319bf5be7c25df56
0824mami <mami0824@gmail.com>
2024-10-24 13:04:34 +0900

    memoApp

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function setValue() {
+    localStorage.getItem("txtMemo");
+    localStorage.getItem("txtMemo");
+    localStorage.getItem("txtMemo");
+    localStorage.getItem("txtMemo");
+
+}
+function setValue() {
+
+    localStorage.getItem("memoField");
+    localStorage.getItem("memoField");
+    localStorage.getItem("memoField");
+}
